ccf32df6358ceec5ae67d73ea4685794070af153
K1r1Cakes <akiriachetty4@gmail.com>
2025-08-17 17:07:46 +0200

    Game Scene | imported terrain asset | Imported AIDialogue scripts

---
+++ Starship Salvage/Assets/Scripts/Spaceship_Dialogue/SpaceshipAIController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpaceshipAIController : MonoBehaviour
+{
+    public static SpaceshipAIController Instance { get; private set; } //Singleton instance allows us to call the scc=ripts from any where
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject); //Make sure only one instance of the dialogue controller 
+    }
+
+    public void ShowDialoguePanel(bool show)
+    {
+        dialoguePanel.SetActive(show); //Toggle panel visibility
+    }
+
+    public void SetShipInfo(string shipName)
+    {
+        nameText.text = shipName;
+    }
+
+    public void SetDialogue(string text)
+    {
+        dialogueText.text = text;
+    }
+
+}
---
+++ Starship Salvage/Assets/Scripts/Spaceship_Dialogue/SpaceshipDialogue.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewSpaceshipDialogue", menuName = "Spaceship Dialogue")]
+public class SpaceshipDialogue : ScriptableObject
+{
+    public string shipName;
+    public string[] Lines; //Actual Spaceship dialogue
+    public bool[] autoProgressLines; // Moves on to the Next line
+    public bool[] endLines; //Dialogue ends
+    public float autoProgressDelay; //Delay between dialogue
+    public float typingSpeed; //Speed at which each char is displayed
+
+}
